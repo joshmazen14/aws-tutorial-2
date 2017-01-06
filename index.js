cce0890a5a4828d0e3f5b28620aac08fdd9ab9ba
Josh Mazen <joshmazen@Josh-Mazen-258856.local>
2017-01-06 13:16:52 -0500

    Serving public files

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Here come dat code.')
-})
-
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
