9ff1fdf835d2e4f8e5b07799d8ea663cacd60a32
Josh Mazen <joshmazen@Josh-Mazen-258856.local>
2017-01-06 13:00:55 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+        name: 'aws-tutorial-2',
+        script: './index.js'
+    }],
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            host: 'ec2-35-165-81-182.us-west-2.compute.amazonaws.com',
+            key: '~/.ssh/Tutorial.pem',
+            ref: 'origin/master',
+            repo: 'git@github.com:joshmazen14/aws-tutorial-2.git',
+            path: '/home/ubuntu/aws-tutorial-2',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        }
+    }
+}
