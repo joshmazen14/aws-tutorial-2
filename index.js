c4f0540d82ff1bbc613700ef4f316bea94fce891
Josh Mazen <joshmazen@Josh-Mazen-258856.local>
2017-02-23 16:22:50 -0500

    log visitors

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const express = require('express')
-const app = express()
+const { MongoClient } = require('mongodb')
+const mongoUrl = 'mongodb://localhost:27017/tutorial-app'
 
-app.use(express.static('public'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+MongoClient.connect(mongoUrl, (err, db) => {
+  if (err) return console.log('Failed to connect to MongoDB', err)
+  const app = express()
+  const logs = db.collection('logs')
+
+  app.use(express.static('public'))
+  app.get('/log', (req, res) => {
+    logs.insertOne({
+        ts: Date.now(),
+        ua: req.headers['user-agent'],
+        referrer: req.headers['referer'],
+        ip: getIp(req)
+    }, (err) => {
+        if (err) {
+            console.log('Failed to create log')
+            throw err
+        }
+        res.send('You just got tracked, bro')
+        })
+    })
+    app.listen(3000, () => console.log('Server running on port 3000'))
+})
+
+function getIp (req) {
+  return req.headers['x-forwarded-for'] || req.connection.remoteAddress
+}
